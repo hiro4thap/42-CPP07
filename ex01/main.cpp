b046658bded70113fe40bbc208f01c570fec2c0d
Hiroaki Ono <hiono@student.42adel.org.au>
2024-08-12 19:08:54 +0930

    test case added

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,25 +1,43 @@
 #include "iter.hpp"
 #include "Log.hpp"
 
-void	double_int(int &i)
+template<typename T> void	increment(T &i)
 {
-	i *= 2;
+	i += 1;
 }
 
 int	main()
 {
 	std::size_t	size = 5;
-	int	array[5] = {1, -3, 15, 10, 2};
+	int	int_array[5] = {1, -3, 15, 10, 2};
 
-	Log::nl("Before double_int", YELLOW);
+	Log::nl("Int Array", CYAN);
+	Log::nl("Before increment", YELLOW);
 	for (std::size_t i = 0 ; i < size; i++)
-		std::cout << array[i] << "\n";
+		std::cout << int_array[i] << "\n";
 
-	iter(array, size, &double_int);
+	iter(int_array, size, &increment);
 
 	Log::nl();
-	Log::nl("After double_int", YELLOW);
+	Log::nl("After increment", YELLOW);
 	for (std::size_t i = 0 ; i < size; i++)
-		std::cout << array[i] << "\n";
+		std::cout << int_array[i] << "\n";
+
+	Log::nl();
+	char	char_array[5] = {'a', 't', 'M', '/', '9'};
+
+	Log::nl("Char Array", CYAN);
+	Log::nl("Before increment", YELLOW);
+	for (std::size_t i = 0 ; i < size; i++)
+		std::cout << char_array[i] << "\n";
+
+	iter(char_array, size, &increment);
+
+	Log::nl();
+	Log::nl("After increment", YELLOW);
+	for (std::size_t i = 0 ; i < size; i++)
+		std::cout << char_array[i] << "\n";
+
+
 	return 0;
 }
